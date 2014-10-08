e60f29cac59b6bd380c59b9e3e24e264768bd7ff
betahikaru <beta.hikaru2000+github@gmail.com>
2014-10-09 01:17:47 +0900

    Add sub command 'create_task'

--- lib/todo/command.rb
+++ lib/todo/command.rb
@@ -3,6 +3,20 @@ module Todo
   # コマンドラインベースの処理を行うクラスです
   # @author betahikaru
   class Command
+
+    # メイン処理
+    def execute
+      DB.prepare
+    end
+
+    # タスクの新規作成
+    # @param [String] name
+    # @param [String] content
+    def create_task(name, content)
+      # statusはデフォルト(NOT_YET)
+      Task.create!(name: name, content: content).reload
+    end
+
   end
 
 end
