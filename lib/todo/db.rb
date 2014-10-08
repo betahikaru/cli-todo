07e9318de28c358bb2d79ebcc42824bb28a29566
betahikaru <beta.hikaru2000+github@gmail.com>
2014-10-08 23:25:51 +0900

    Create DB file and :tasks table

--- lib/todo/db.rb
+++ lib/todo/db.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 require 'active_record'
 
 module Todo
@@ -9,8 +10,48 @@ module Todo
     # データベースへの接続とテーブルの作成を行う
     # @return [void]
     def self.prepare
+      database_path = File.join(ENV['HOME'], '.todo', 'todo.sqlite3')
+
+      connect_database database_path
+      create_table_if_not_exists database_path
     end
 
+    # @param [String] path データベースファイルのパス
+    def self.connect_database(path)
+      spec = {
+        adapter: 'sqlite3',
+        database: path
+      }
+      ActiveRecord::Base.establish_connection spec
+    end
+
+    # @param [String] path データベースファイルのパス
+    def self.create_table_if_not_exists(path)
+      create_database_path path
+
+      connection = ActiveRecord::Base.connection
+
+      # テーブルが存在する場合は終了
+      return if connection.table_exists?(:tasks)
+
+      connection.create_table :tasks do |t|
+        t.column :name, :string, null: false
+        t.column :content, :text, null: false
+        t.column :status, :integer, default: 0, null:false
+        t.timestamps
+      end
+
+      connection.add_index :tasks, :status
+      connection.add_index :tasks, :created_at
+    end
+
+    # @param [String] path データベースファイルのパス
+    def self.create_database_path(path)
+      FileUtils.mkdir_p File.dirname(path)
+    end
+
+    private_class_method :connect_database, :create_table_if_not_exists,
+      :create_database_path
   end
 
 end
